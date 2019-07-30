65e3c1dec3eedd80672d6743abcd2a839ccb29fe
l0n3woof <shikharsaxena10@gmail.com>
2019-07-30 20:21:19 +0530

    heroku push

--- autocomplete/autocomplete/settings.py
+++ autocomplete/autocomplete/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '89^g_iuyvvq&1g6m@it=3o98xx$!=&&qpylv!$hl=+5ax$sj61'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fuzzysearch95.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -40,7 +40,15 @@ INSTALLED_APPS = [
     'search'
 ]
 
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +59,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'autocomplete.urls'
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 TEMPLATES = [
     {
